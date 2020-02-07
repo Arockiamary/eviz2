aa9dd1df0d0c1c408da56c31fc5eaaaaa7a93070
Arockiamary <marockiamary97@gmail.com>
2020-02-07 13:55:06 +0530

    Navigation bar changed.

---
+++ nav.php
@@ -0,0 +1,85 @@
+<?php
+$navi ='<header> 
+    
+    <!-- Top bar -->
+    <div class="top-bar">
+      <div class="top-info">
+        <div class="container">
+          <ul class="personal-info">
+            <li>
+              <p><i class="fa fa-phone"></i> +1 123 456 7890 </p>
+            </li>
+            <li>
+              <p>Hi! Here comes custom txt line </p>
+            </li>
+            <li>
+              <p>example@example.com </p>
+            </li>
+          </ul>
+          
+          <!-- Right Sec -->
+          <div class="right-sec"> 
+            
+            <!-- Language -->
+            <select class="selectpicker">
+              <option>English</option>
+              <option>French</option>
+              <option>Relish</option>
+            </select>
+            
+            <!-- social -->
+            <ul class="social">
+              <li><a href="#."><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#."><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#."><i class="fa fa-google"></i></a></li>
+              <li><a href="#."><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <!-- Navigation -->
+    <nav class="navbar">
+      <div class="sticky">
+        <div class="container"> 
+          
+          <!-- LOGO -->
+          <div class="logo"> <a href="index.php"><img  class="img-responsive" src="images/logo.png" alt="" ></a> </div>
+          
+          <!-- Nav -->
+          <ul class="nav ownmenu">
+            <li class="active"> <a href="index.php">Home </a> </li>
+           
+            <li> <a href="about.php">About Us </a> </li>
+            <li> <a href="services(ver1).php">Services </a>  
+              <ul class="dropdown">
+                <li> <a href="services(ver1).php">Manage Service Provider</a> </li>
+                <li> <a href="services(ver2).php">Cloud Migration</a> </li>
+                <li> <a href="services(ver3).php">Salesforces</a> </li>
+                <li> <a href="services(ver4).php">Mobile Application (iOS &Andriod)</a> </li>
+                <li> <a href="services(ver5).php">IoT</a> </li>
+                <li> <a href="services(stories).php">Custom App Development</a> </li>
+                <li> <a href="services(stories).php">Web Application</a> </li>
+                <li> <a href="services(annual-reports)_draft02.php">Web Sites</a> </li>
+                <li> <a href="services(media-relations)_draft02.php">Consultant</a> </li>
+                <li> <a href="services-left-sidebar(annual-report-single)_draft02.php">Design</a> </li>
+                
+              </ul>
+            </li>
+            
+            <li> <a href="blog.php">Clients </a></li>
+            <li> <a href="contact.php">Contact Us</a> </li>
+          </ul>
+          <!-- Search -->
+          <div class="search-icon"> <a href="#."><i class="fa fa-search"></i></a>
+            <form>
+              <input class="form-control" type="search" placeholder="Type Here">
+              <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>';
+  ?>
